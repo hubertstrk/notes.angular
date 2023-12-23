b07db04c56fa51306d44d8a654d14585e8c053f3
Hubert Stärk <hubert.staerk@gmail.com>
2023-12-23 15:30:42 +0100

    add prime/primeflex/primeicons

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { SidebarModule } from 'primeng/sidebar';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [ButtonModule, SidebarModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
