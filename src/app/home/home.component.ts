706ef4aa3a887e6dc84d6902b60c6d3b225e2838
Hubert Stärk <hubert.staerk@gmail.com>
2025-05-01 21:36:43 +0200

    Auto stash before rebase of "development" onto "origin/development"

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,19 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { EditorComponent } from '../editor/editor.component';
-import { PreviewComponent } from '../preview/preview.component';
-import { SidebarComponent } from '../sidebar/sidebar.component';
-import { HeaderComponent } from '../header/header.component';
 import { EditorChange } from '../../model/editor';
 import { SettingsService } from '../../service/settings-service';
+import { CommonModule } from '@angular/common';
+
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [
-    HeaderComponent,
-    SidebarComponent,
-    EditorComponent,
-    PreviewComponent,
-  ],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
@@ -21,6 +14,8 @@ export class HomeComponent implements OnInit {
   editorChange: EditorChange;
 
   constructor(private settingsService: SettingsService) {}
+
+  collapsed = false;
 
   onTextChanged(event: EditorChange) {
     // TODO: save note
