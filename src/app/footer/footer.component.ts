b5bcd872ced5ddf9d99befc96beebb147542647e
Hubert Stärk <hubert.staerk@gmail.com>
2025-06-07 21:23:45 +0200

    add unsubscribe mechanism to FooterComponent to prevent memory leaks

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { selectActiveNote } from '@store/note/note.selectors';
 import { selectCursorPosition } from '@store/editor/editor.selectors';
 import { CommonModule } from '@angular/common';
 import { Note } from '@models/note.model';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-footer',
@@ -11,20 +12,27 @@ import { Note } from '@models/note.model';
   standalone: true,
   imports: [CommonModule],
 })
-export class FooterComponent implements OnInit {
+export class FooterComponent implements OnInit, OnDestroy {
   activeNote$ = this.store.select(selectActiveNote);
   cursorPosition$ = this.store.select(selectCursorPosition);
   activeNote: Note | null = null;
   cursorPosition: { line: number; column: number } = { line: 0, column: 0 };
 
+  private destroy$ = new Subject<void>();
+
   constructor(private store: Store) {}
 
   ngOnInit(): void {
-    this.activeNote$.subscribe(note => {
+    this.activeNote$.pipe(takeUntil(this.destroy$)).subscribe(note => {
       this.activeNote = note;
     });
-    this.cursorPosition$.subscribe(position => {
+    this.cursorPosition$.pipe(takeUntil(this.destroy$)).subscribe(position => {
       this.cursorPosition = position;
     });
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 }
